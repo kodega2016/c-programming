0c35bb5447851bd4d5d77704d9a3c7327a59320a
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2024-11-02 15:03:32 +0545

    added string formatter to format string

--- string_formatter/main.c
+++ string_formatter/main.c
@@ -4,6 +4,7 @@ int main(void){
     char name[]="Khadga Bahadur Shrestha";
     int age=27;
     float salary=20000.0;
-    printf("My name is %s, my age is %d and my salary is %f\n",name,age,salary);
+    float bonus=salary/2;
+    printf("My name is %s, my age is %d and my salary is %f and bonus is %.2f\n",name,age,salary,bonus);
     return 0;
 }
