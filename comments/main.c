187ff96dc1b6db9c92624874a74ef28f4d1c6285
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2024-11-02 14:45:26 +0545

    added comments in c

---
+++ comments/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void){
+    // this is a single line comment
+    /*
+        This is multi line
+        comments for c programming
+    */
+    printf("this is a simple program");
+    return 0;
+}
